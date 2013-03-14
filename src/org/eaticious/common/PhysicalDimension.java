72abb8fb54cd57946eccd8cfb66ceec3ef767660
Sven Peetz <sven@greenlicious.org>
2013-03-14 18:13:18 +0100

    added comments added GHG (greenhouse gas emissions) as a new PhysicalDimension

--- src/org/eaticious/common/PhysicalDimension.java
+++ src/org/eaticious/common/PhysicalDimension.java
@@ -2,14 +2,25 @@ package org.eaticious.common;
 
 public enum PhysicalDimension {
 
-	MASS(Unit.KILOGRAM), VOLUME(Unit.LITRE), DISTANCE(Unit.KILOMETER), ENERGY(Unit.KILOWATTHOUR), OTHER(Unit.NONE);
+	MASS(Unit.KILOGRAM), VOLUME(Unit.LITRE), DISTANCE(Unit.KILOMETER), ENERGY(Unit.KILOWATTHOUR), OTHER(Unit.NONE), GHG(Unit.CO2E);
 	
+	/**
+	 * The standard Unit for this PhysicalDimension of this
+	 */
 	private Unit baseUnit;
 	
+	/**
+	 * 
+	 * @param baseunit The standard Unit for the PhysicalDimension to be constructed
+	 */
 	private PhysicalDimension(Unit baseunit){
 		this.baseUnit = baseunit;
 	}
 	
+	/**
+	 * Returns the standard Unit of this PhysicalDimension
+	 * @return the standard Unit of this PhysicalDimension
+	 */
 	public Unit getBaseUnit(){
 		return this.baseUnit;
 	}
