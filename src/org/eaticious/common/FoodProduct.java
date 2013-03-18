e7b0f7241984cc38d42e6451c21c528cdc890738
Sven Peetz <sven@greenlicious.org>
2013-03-18 17:20:34 +0100

    adding comments to meaning of bulk, volume and average

--- src/org/eaticious/common/FoodProduct.java
+++ src/org/eaticious/common/FoodProduct.java
@@ -8,15 +8,24 @@ import java.util.Map;
 
 public interface FoodProduct extends Serializable {
 
-	// APPROVE Aurelian: check if this is ok with you
-	// Aurelian: Yes it is, could you specify what the range of the classes are? probably via density?
+	/**
+	 * Describing classes of goods regarding the transport
+	 * 
+	 * BULK: Goods such as coal, grains, or bananas that are not packaged in any type of container and are stored,
+	 * transported, and sold in large quantities. Transporting these goods normally will come with a high capacity
+	 * utilization of the vessel.
+	 * 
+	 * VOLUME: Goods such as cars, vehicle parts, etc that take a lot of volume within the vessel and lead to a low
+	 * capacity utilization
+	 * 
+	 * AVERAGE: statistically determined average value for all transports of a given carrier in a reference year
+	 */
 	public enum WeightClass {
 		BULK, AVERAGE, VOLUME, UNKNOWN;
 	}
 
 	/**
-	 * Returns the name of this FoodProduct in the requested language as a
-	 * String or null if no translation is available
+	 * Returns the name of this FoodProduct in the requested language as a String or null if no translation is available
 	 * 
 	 * @param locale
 	 *            Locale holding the requested language
@@ -25,22 +34,19 @@ public interface FoodProduct extends Serializable {
 	public String getName(Locale locale);
 
 	/**
-	 * @return The code of this product in the requested product classification
-	 *         or null if unknown
+	 * @return The code of this product in the requested product classification or null if unknown
 	 */
 	public String getCode(ProductClassification classification);
 
 	/**
 	 * 
-	 * @return A Map holding amounts of nutrients per serving of the associated
-	 *         recipe
+	 * @return A Map holding amounts of nutrients per serving of the associated recipe
 	 */
 	public Map<Nutrient, Quantity> getNutritionData();
 
 	/**
 	 * 
-	 * @return A List of all the food products which can be used in a recipe to
-	 *         replace this product
+	 * @return A List of all the food products which can be used in a recipe to replace this product
 	 */
 	public List<FoodProduct> getSubstitutes();
 
@@ -50,8 +56,7 @@ public interface FoodProduct extends Serializable {
 	 *            The region for which the season is requested
 	 * @param date
 	 *            The date for which the season should be determined
-	 * @return The season in the requested region at the requested date or null
-	 *         if unknown
+	 * @return The season in the requested region at the requested date or null if unknown
 	 */
 	public Season getSeason(Region region, Date date);
 
@@ -59,22 +64,19 @@ public interface FoodProduct extends Serializable {
 	 * 
 	 * @param region
 	 *            The region for which the seasons are requested
-	 * @return A List with all known production seasons for the ingredient in
-	 *         the requested country
+	 * @return A List with all known production seasons for the ingredient in the requested country
 	 */
 	public List<Season> getSeasons(Region region);
 
 	/**
 	 * 
-	 * @return density of ingredient for correct volume to weight transformation
-	 *         (EL, TL to grams)
+	 * @return density of ingredient for correct volume to weight transformation (EL, TL to grams)
 	 */
 	public Double getDensity();
 
 	/**
 	 * 
-	 * @return A list of tags describing any parameter of this product (e.g.
-	 *         contains gluten, lactose...)
+	 * @return A list of tags describing any parameter of this product (e.g. contains gluten, lactose...)
 	 */
 	public List<String> getTags();
 
@@ -88,15 +90,13 @@ public interface FoodProduct extends Serializable {
 	 * 
 	 * @param consumer
 	 *            The consuming region
-	 * @return A Map holding all known producers for this product in the
-	 *         consuming region and the corresponding percentage the producer
-	 *         adds to the consumer's market
+	 * @return A Map holding all known producers for this product in the consuming region and the corresponding
+	 *         percentage the producer adds to the consumer's market
 	 */
 	public Map<Region, Integer> getProducingRegions(Region consumer);
 
 	/**
-	 * The scientific name of the product or null if the scientific name is not
-	 * known
+	 * The scientific name of the product or null if the scientific name is not known
 	 * 
 	 * @return
 	 */
@@ -109,8 +109,7 @@ public interface FoodProduct extends Serializable {
 	public String getSynonyms();
 
 	/**
-	 * @return A String giving additional information about what to bear in mind
-	 *         when buying this product
+	 * @return A String giving additional information about what to bear in mind when buying this product
 	 */
 	public String getShoppingInfo();
 
@@ -119,13 +118,10 @@ public interface FoodProduct extends Serializable {
 	public String getCategory();
 
 	/**
-	 * APPROVE Aurelian: check if this is ok with you
-	 * Aurelian: see above
-	 * Returns the WeightClass (BULK, AVERAGE, VOLUME) of this FoodProduct. If
-	 * the WeightClass is not known UNKNOWN will be returned.
+	 * APPROVE Aurelian: check if this is ok with you Aurelian: see above Returns the WeightClass (BULK, AVERAGE,
+	 * VOLUME) of this FoodProduct. If the WeightClass is not known UNKNOWN will be returned.
 	 * 
-	 * @return The WeightClass of this FoodProduct or WeightClass.UNKNOWN if the
-	 *         weightclass is not specified
+	 * @return The WeightClass of this FoodProduct or WeightClass.UNKNOWN if the weightclass is not specified
 	 */
 	public WeightClass getWeightClass();
 
